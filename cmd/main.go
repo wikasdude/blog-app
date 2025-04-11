8f00eb38bf36292e1305d85851c7e12e77f152c1
wikasdude <kuki.sharma786@gmail.com>
2025-04-11 12:10:47 +0530

    changes done

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
+// @host blog-app-api-7t7q.onrender.com:8080
 // @BasePath /
 
 // @securityDefinitions.apikey BearerAuth

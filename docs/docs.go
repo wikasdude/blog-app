b4e74dd6a8adf36f4f90b9af337e304f45baf156
wikasdude <kuki.sharma786@gmail.com>
2025-04-11 12:04:35 +0530

    docs added

--- docs/docs.go
+++ docs/docs.go
@@ -635,7 +635,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "https://blog-app-api-7t7q.onrender.com:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Blog API",

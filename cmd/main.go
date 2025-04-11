6be69d863f7a9f2b025b68d03e8a5bf9c263125c
wikasdude <kuki.sharma786@gmail.com>
2025-04-11 12:07:34 +0530

    docs added

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,6 @@
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host https://blog-app-api-7t7q.onrender.com:8080
 // @BasePath /
 
 // @securityDefinitions.apikey BearerAuth

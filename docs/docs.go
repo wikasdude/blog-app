02334e3ff40d10a612d8173532eefbb0d34e2dc3
wikasdude <kuki.sharma786@gmail.com>
2025-04-11 12:25:33 +0530

    port removed

--- docs/docs.go
+++ docs/docs.go
@@ -635,7 +635,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "blog-app-api-7t7q.onrender.com:8080",
+	Host:             "blog-app-api-7t7q.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{"https"},
 	Title:            "Blog API",
